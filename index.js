3bd33a681f13880e353a4fb4ae0cbe09702e494d
ayush kumar <kayush242@gmail.com>
2020-02-20 19:28:31 +0530

    set up view engine

--- index.js
+++ index.js
@@ -5,6 +5,11 @@
  //use express router
  app.use('/',require('./routes'));
 
+
+ //set up the view engine
+ app.set('view engine','ejs');
+ app.set('views', './views');
+
  app.listen(port, (err) => {
      if(err){
     
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express= require('express');
 const  router = express.Router();
 const homecontroller=require('../controllers/home_controller');
-
+ 
 console.log('router loaded');
+
+
     
 router.get('/',homecontroller.home);
+router.use('/user',require('./user'));
 
 module.exports=router;
