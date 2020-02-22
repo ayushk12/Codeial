dbfb10e6cb5151c2e5bc0b70be0c1deacab696cb
ayush kumar <kayush242@gmail.com>
2020-02-22 14:14:59 +0530

    created sign and signup page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,51 @@
+const user= require('../models/user');
 module.exports.profile= function(req,res){
-    res.end('<h1>User profile</h1>');
+     return res.end('user_profile',{
+         title: 'User Profile'
+     })
+}
+
+
+//render the sign up page
+module.exports.signup=function(req,res){
+    return res.render('user_sing_up',{
+        title: "Cddeial | Sign up"
+    })
+}
+
+
+//render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sing_in',{
+        title: "Cddeial | Sign In"
+    })
+}
+
+// get the sign up data
+
+module.exports.create= function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return  res.redirect('back');
+    }
+    user.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('error in finding in signing up');
+        return;
+    }
+    if(!user){
+        user.create(req.body,function(err,user){
+            if(err){console.log('error  in cresting user while up');return}
+            return  res.redirect('/users/sign-in');
+        })
+    }   else{
+        return  res.redirect('value');
+    }
+    })
+    
+}
+
+// sign in and create a session for the user
+module.exports.createSession=function(req,res){
+    return res.render('user_sing_up',{
+        title: "Cddeial | Sign Up"
+    })
 }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
  const express = require('express');
  const app=express();
+ const cookieParser= require('cookie-parser');
  const port=8000;
  const expressLayouts=require('express-ejs-layouts');
 
+//reading the cookies
+
+app.use(express.urlencoded());
+app.use(cookieParser());// calling cookie
  //connection for database
  const db=require('./config/mongoose');
  app.use(express.static('./assets'))
